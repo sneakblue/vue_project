7e312579cc9ca8fd7167a0bda98b8f6888ca5ec6
david <sneakblue@gmail.com>
2022-03-31 17:20:38 -0600

    adding pinia, latest dist of vuex

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,18 +1,21 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+    name: 'App',
   components: {
-    HelloWorld
+      HelloWorld
   }
 }
 </script>
+
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
 
 <style>
 #app {
