3b6c96856011df452dd4e2b2ca213b7b8b80cac6
david <sneakblue@gmail.com>
2022-04-02 23:41:11 -0600

    login form created and functional

--- vue-app/src/components/auth/LoginForm.vue
+++ vue-app/src/components/auth/LoginForm.vue
@@ -1,10 +1,48 @@
 <script setup name='LoginForm'>
+import { ref } from 'vue'
 import appStore from '../../stores/index.js'
 
 const sessionStore = appStore.useSessionStore;
 console.log(sessionStore.user);
+
+const email = ref('');
+const password = ref('');
+
+const handleLogin = async () => {
+    console.log(email);
+    console.log(password);
+    await sessionStore.login(email.value, password.value);
+    if (sessionStore.user) {
+        this.router.push('/');
+    }
+}
+
+// const updateEmail = (event) => {
+//     email = event.target.value;
+// }
+
+// const updatePassword = (event) => {
+//     password = event.target.value;
+// }
+
 </script>
 
 <template>
-    <h4>Login Form Loaded</h4>
+    <div>
+        <form @submit.prevent='handleLogin'>
+            <input type='text'
+                v-model='email'
+                placeholder="email"
+            />
+            <input type='text'
+                v-model='password'
+                placeholder="password"
+            />
+            <button
+                type='submit'
+            >
+                Submit
+            </button>
+        </form>
+    </div>
 </template>
