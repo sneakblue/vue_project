24994269b5c9bff4c2fc2f4124003727f6401eda
david <sneakblue@gmail.com>
2022-04-05 11:52:31 -0600

    trying to edit publicPath in the vue.config.js file to add terniary set to production

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
       proxy: 'http://localhost:5000'
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'Production' ? 'https://fortestapping.herokuapp.com' : '/'
 })
